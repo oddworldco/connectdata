237daf25eabc23fede67740a74b6709113d367f7
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2017-06-24 11:51:10 -0500

    update

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var monk = require('monk');
 // var db = monk('localhost:27017/nodetest1');
-var db = 
+var db = 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASSWORD + '@ds157349.mlab.com:57349/homechef_alexa_spike'
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
